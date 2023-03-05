d970f65d28bb12d222a46d8232430da506d9c4ca
NikolaGolubovic <nikolagolubovic985@gmail.com>
2023-03-05 22:32:41 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AllQuestions from "./components/AllQuestions";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
+import Notes from "./components/Notes";
+import Quiz from "./components/Quiz";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  function notify(msg: string, type: string): void {
+    if (type === "success") {
+      console.log("hello from notify");
+      let notifyFunc = () => toast["success"](msg, { position: toast.POSITION.TOP_CENTER });
+      notifyFunc();
+    }
+    if (type === "error") {
+      console.log("hello from notify");
+      let notifyFunc = () => toast["error"](msg, { position: toast.POSITION.TOP_CENTER });
+      notifyFunc();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer autoClose={1500} />
+      <Navigation />
+      <Routes>
+        {" "}
+        <Route path="/practice" element={<AllQuestions notify={notify} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login notify={notify} />} />
+        <Route path="/notes" element={<Notes notify={notify} />} />
+        <Route path="/quiz" element={<Quiz notify={notify} />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
